427855fc6c9acd0b9ea831aa050f5d8f1a348866
titi14gj <titi14gj@gmail.com>
2026-05-10 12:21:54 +0800

    Expose settings event to renderer

--- electron_app/src/preload.js
+++ electron_app/src/preload.js
@@ -4,5 +4,9 @@ contextBridge.exposeInMainWorld('desktopPet', {
   chooseMedia: () => ipcRenderer.invoke('choose-media'),
   setWindowSize: (size) => ipcRenderer.invoke('set-window-size', size),
   setAlwaysOnTop: (enabled) => ipcRenderer.invoke('set-always-on-top', enabled),
-  quit: () => ipcRenderer.invoke('quit-app')
+  quit: () => ipcRenderer.invoke('quit-app'),
+  onShowSettings: (callback) => {
+    if (typeof callback !== 'function') return;
+    ipcRenderer.on('show-settings', callback);
+  }
 });
